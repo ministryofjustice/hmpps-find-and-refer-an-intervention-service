d3fdc65f7cd5d1977fea93ba263e7e959284141a
Ryan Forsyth <ryan.forsyth@digital.justice.gov.uk>
2024-12-06 10:48:24 +0000

    revert changes (#16)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/integration/IntegrationTestBase.kt
@@ -13,7 +13,7 @@ import uk.gov.justice.hmpps.test.kotlin.auth.JwtAuthorisationHelper
 
 @ExtendWith(HmppsAuthApiExtension::class) // , ExampleApiExtension::class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@ActiveProfiles("integration")
+@ActiveProfiles("test")
 abstract class IntegrationTestBase {
 
   @Suppress("SpringJavaInjectionPointsAutowiringInspection")
