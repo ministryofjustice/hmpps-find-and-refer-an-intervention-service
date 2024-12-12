f4d78e31dc3b9c5304bd2d475f310bdfd5cc941b
amol phalnikar <2399527+amolphalnikar@users.noreply.github.com>
2024-12-12 11:51:11 +0000

    Added dummy table and data as part of migration

---
+++ src/main/resources/db/migration/V1_1_1__dummy_table_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO dummy_table VALUES (1, 'dummy text 1',  now());
+INSERT INTO dummy_table VALUES (2, 'dummy text 2',  now());
+INSERT INTO dummy_table VALUES (3, 'dummy text 3',  now());
+INSERT INTO dummy_table VALUES (4, 'dummy text 4',  now());
+INSERT INTO dummy_table VALUES (5, 'dummy text 5',  now());
+INSERT INTO dummy_table VALUES (6, 'dummy text 6',  now());
+INSERT INTO dummy_table VALUES (7, 'dummy text 7',  now());
+INSERT INTO dummy_table VALUES (8, 'dummy text 8',  now());
+INSERT INTO dummy_table VALUES (9, 'dummy text 9',  now());
+INSERT INTO dummy_table VALUES (10, 'dummy text 10',  now());
+
+
