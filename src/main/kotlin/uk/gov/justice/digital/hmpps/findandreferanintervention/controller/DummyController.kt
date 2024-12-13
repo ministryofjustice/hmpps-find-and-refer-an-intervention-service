8de5f777fdc211db3629930d32328ae45f25a7b0
Jack Dempster <jack.dempster@justice.gov.uk>
2024-12-13 12:33:41 +0000

    add preauth annotation to new controller

--- src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/controller/DummyController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/controller/DummyController.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.findandreferanintervention.controller
 
 import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -8,6 +9,7 @@ import uk.gov.justice.digital.hmpps.findandreferanintervention.jpa.entity.Dummy
 import uk.gov.justice.digital.hmpps.findandreferanintervention.jpa.repository.DummyRepository
 
 @RestController
+@PreAuthorize("hasRole('ROLE_INTERVENTIONS_API_READ_ALL')")
 class DummyController(
   private val dummyRepository: DummyRepository,
 ) {
