bb3346def9c3836522e83cd5cf96d937849e7a5f
Jack Dempster <5928690+Jack-Dempster@users.noreply.github.com>
2024-12-17 09:32:58 +0000

    FRI-23: Pipe Cleaning (#22)
    * Add dummy JPA repository and controller
    
    * add preauth annotation to new controller

--- src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/jpa/repository/AuthUserRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/jpa/repository/AuthUserRepository.kt
@@ -1,7 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsfindandreferaninterventionsservice.jpa.repository
 
 import org.springframework.data.repository.CrudRepository
-import uk.gov.justice.digital.hmpps.hmppsfindandreferaninterventionsservice.jpa.entity.AuthUser
+import uk.gov.justice.digital.hmpps.findandreferanintervention.jpa.entity.AuthUser
 
 interface AuthUserRepository : CrudRepository<AuthUser, String> {
   fun findByUserName(userName: String?): AuthUser?
