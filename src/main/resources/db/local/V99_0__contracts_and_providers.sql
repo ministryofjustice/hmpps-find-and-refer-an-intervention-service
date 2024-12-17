c97ede501a40c39cf6ee61e18867fe2b505cebf2
Andrew Hodgson <andrew.hodgson@digital.justice.gov.uk>
2024-12-17 14:53:00 +0000

    Add reference data tables and client tables to allow refer of CRS interventions

---
+++ src/main/resources/db/local/V99_0__contracts_and_providers.sql
@@ -0,0 +1,68 @@
+insert into service_provider(id, name)
+values ('HARMONY_LIVING', 'Harmony Living'),
+       ('HOME_TRUST', 'Home Trust'),
+       ('HELPING_HANDS', 'Helping Hands');
+INSERT INTO contract_type (id,name,code) VALUES
+	 ('72e60faf-b8e5-4699-9d7c-aef631cca71b','Accommodation','ACC'),
+	 ('b402486d-991e-4977-9291-073a3526d60f','Education, Training and Employment (ETE)','ETE'),
+	 ('f9b59d2c-c60b-4eb0-8469-04c975d2e2ee','Personal Wellbeing','PWB'),
+	 ('b74c3f1d-2c53-45c0-bd23-d61befaf00af','Women''s Services','WOS'),
+	 ('bc65434d-089b-4c37-80c2-50efebb76933','Mentoring','MTR'),
+	 ('5a7d2856-2126-478d-ab12-afcc07deb977','Dependency and Recovery','DNR'),
+	 ('a93c152c-ed56-48f9-92e8-401ff7aa2fa8','Women''s Support Services (GM)','WSM'),
+	 ('5aa47f40-5f98-4300-a274-f8272c2f127d','Finance, Benefit and Debt (East Midlands)','FBD-EM'),
+	 ('4c29a22e-39a5-41ef-a8e4-69c8507ae058','Finance, Benefit and Debt (West Midlands)','FBD-WM'),
+	 ('c15ad3f3-f24b-4cd6-8ff6-03df4927fbe9','Finance, Benefit and Debt (London)','FBD-L');
+INSERT INTO contract_type (id,name,code) VALUES
+	 ('f7569c39-5c3a-457e-9a29-435858af55e0','Finance, Benefit and Debt (South Central)','FBD-SC'),
+	 ('c1053875-9492-48b3-94bc-ecde8c249b26','Dependency and Recovery (Yorkshire and the Humber)','DNR-Y'),
+	 ('c8a17e8a-1804-4c68-8109-f58447f5f06d','Dependency and Recovery (London)','DNR-L'),
+	 ('8028db0e-ee90-4982-b894-78f940ebd8c8','Dependency and Recovery (North East)','DNR-NE'),
+	 ('7af3a832-2b46-43b2-9db1-6bc43181a233','Dependency and Recovery (Kent, Surrey and Sussex)','DNR-K'),
+	 ('588fea26-dae1-412e-add6-73e4e6d84a3c','Dependency and Recovery (North East: Northumbria)','DNR-NEN'),
+	 ('5379196d-559a-4709-bf37-fe6a6d65d9b5','Finance, Benefit and Debt (Wales)','FBD-W'),
+	 ('d10c792d-12bd-4ac4-a999-04dbdbf59940','Dependency and Recovery (South Central)','DNR-SC'),
+	 ('bcb3eb1d-b95d-4679-bb85-b91aa3240ef5','Dependency and Recovery (South Yorkshire)','DNR-SY'),
+	 ('a19f3da7-c5eb-4936-9f76-f877ddf5c656','Dependency and Recovery (West Midlands)','DNR-WM');
+INSERT INTO contract_type (id,name,code) VALUES
+	 ('ee20282e-aea4-429e-8b3f-0d868c46e8bc','Finance, Benefit and Debt (Yorkshire and the Humber)','FBD-Y'),
+	 ('bb6b0997-a9bd-4d90-b908-15606f61d230','Finance, Benefit and Debt (North West)','FBD-NW'),
+	 ('69b593ec-705c-4095-afcb-652a1eddb071','Finance, Benefit and Debt (South West)','FBD-SW'),
+	 ('0b83447e-5a15-4bfe-af8e-0650cbe74a49','Dependency and Recovery (South West)','DNR-SW'),
+	 ('8ecbf69b-9b54-49d8-a2bc-54c7b7cc4731','Finance, Benefit and Debt in the North East','FBD-NE'),
+	 ('8cd135aa-c7d9-4f60-be25-81f0fe80e736','Finance, Benefit and Debt (Stockport)','FBD-SP'),
+	 ('d5805712-42a4-44b6-8368-ba701defa9fa','Finance, Benefit and Debt (Bolton)','FBD-B'),
+	 ('17a6abbd-ca97-4aaa-824f-6bae05523d81','Finance, Benefit and Debt (Trafford)','FBD-TR'),
+	 ('42dff63e-1b46-4296-bc56-4b213da84336','Finance, Benefit and Debt (Rochdale)','FBD-R'),
+	 ('68240d7b-58aa-4156-8792-900b212751e2','Finance, Benefit and Debt (Salford)','FBD-SA');
+INSERT INTO contract_type (id,name,code) VALUES
+	 ('26239d61-b742-423a-842c-fab1950e17a2','Finance, Benefit and Debt (Bury)','FBD-BU'),
+	 ('50b1a1cc-d326-4e93-903c-633c9f273ee6','Finance, Benefit and Debt (Manchester)','FBD-M'),
+	 ('15fe353b-8bc1-4e72-940a-44ebad8e3f41','Finance, Benefit and Debt (Tameside)','FBD-TA'),
+	 ('8985ba5b-104b-4329-aa49-df3aa9a7beef','Finance, Benefit and Debt (Wigan)','FBD-WI'),
+	 ('95fbfee1-04a4-4e61-9b3e-c6dffef4fcee','Finance, Benefit and Debt (Oldham)','FBD-O');
+
+insert into dynamic_framework_contract (id, contract_type_id, prime_provider_id, start_date, end_date, nps_region_id, pcc_region_id, allows_female, allows_male, minimum_age, maximum_age, contract_reference)
+values ('1d7f8fcc-aa12-4705-a6a5-0d40467e03e9', '72e60faf-b8e5-4699-9d7c-aef631cca71b', 'HARMONY_LIVING', TO_DATE('2020-12-15', 'YYYY-MM-DD'), TO_DATE('2023-12-15', 'YYYY-MM-DD'), 'G', null, true, true, 18, 25, '0001'),
+       ('f9d24b4a-390d-4cc1-a7ee-3e6f022e1599', '72e60faf-b8e5-4699-9d7c-aef631cca71b', 'HARMONY_LIVING', TO_DATE('2020-01-01', 'YYYY-MM-DD'), TO_DATE('2022-12-31', 'YYYY-MM-DD'), 'G', null, true, false, 18, 25, '0002'),
+       ('24f7a423-15a6-438d-9d28-063e92b25a9b', '72e60faf-b8e5-4699-9d7c-aef631cca71b', 'HARMONY_LIVING', TO_DATE('2021-12-11', 'YYYY-MM-DD'), TO_DATE('2025-12-11', 'YYYY-MM-DD'), null, 'avon-and-somerset', true, true, 25, null, '0003'),
+       ('c7d39f92-6f43-49a4-bb62-e0f42c864765', 'f9b59d2c-c60b-4eb0-8469-04c975d2e2ee', 'HARMONY_LIVING', TO_DATE('2021-01-01', 'YYYY-MM-DD'), TO_DATE('2035-05-01', 'YYYY-MM-DD'), 'G', null, false, true, 18, null, '0004'),
+       ('0b60d842-9c08-408e-8c8d-f6dbf8e5c3f4', 'f9b59d2c-c60b-4eb0-8469-04c975d2e2ee', 'HARMONY_LIVING', TO_DATE('2021-01-01', 'YYYY-MM-DD'), TO_DATE('2035-05-01', 'YYYY-MM-DD'), 'J', null, true, true, 18, null, '0005'),
+       ('56ad7d77-94c7-4fbf-a704-29e0f6ad078f', 'f9b59d2c-c60b-4eb0-8469-04c975d2e2ee', 'HARMONY_LIVING', TO_DATE('2021-01-01', 'YYYY-MM-DD'), TO_DATE('2035-05-01', 'YYYY-MM-DD'), 'A', null, true, true, 18, null, '0006'),
+       ('1435d1c5-0c22-459a-bd1a-ce593fba6c05', 'f9b59d2c-c60b-4eb0-8469-04c975d2e2ee', 'HOME_TRUST', TO_DATE('2021-01-01', 'YYYY-MM-DD'), TO_DATE('2035-05-01', 'YYYY-MM-DD'), 'A', null, true, true, 18, null, '0007');
+
+insert into intervention (id, dynamic_framework_contract_id, created_at, title, description, incoming_referral_distribution_email)
+values ('98a42c61-c30f-4beb-8062-04033c376e2d', '1d7f8fcc-aa12-4705-a6a5-0d40467e03e9', TO_DATE('2020-10-15', 'YYYY-MM-DD'), 'Accommodation Service', 'The service aims are to support in securing settled accommodation.', 'contact@harmonyliving.com'),
+       ('90e35306-5d65-4901-bb36-c4c44c7d19f9', 'f9d24b4a-390d-4cc1-a7ee-3e6f022e1599', TO_DATE('2021-01-15', 'YYYY-MM-DD'), 'Sheltered Accommodation', 'Providing help to move female service users into safe accommodation.', 'contact@harmonyliving.com'),
+       ('08524319-7d5b-4b56-862a-bfe2c9a545f5', '24f7a423-15a6-438d-9d28-063e92b25a9b', TO_DATE('2021-02-01', 'YYYY-MM-DD'), 'Accommodation/Good Tenant', 'This course offers practical information and advice helping participants understand what it means to be a ''good'' tenant.', 'contact@harmonyliving.com'),
+       ('f803445f-326c-4ef8-aee2-f7716d417832', 'c7d39f92-6f43-49a4-bb62-e0f42c864765', TO_DATE('2020-11-11', 'YYYY-MM-DD'), 'Social Inclusion', 'This programme is aimed at males who have offended.
+
+• This programme is aimed at males who have offended.
+• This is some more text about the intervention', 'contact@hometrust.com'),
+       ('15237ae5-a017-4de6-a033-abf350f14d99', '0b60d842-9c08-408e-8c8d-f6dbf8e5c3f4', TO_DATE('2020-11-11', 'YYYY-MM-DD'), 'Begins at Home', 'This service is dedicated for handling challenging situations at home.', 'contact@hometrust.com'),
+       ('3ccb511b-89b2-42f7-803b-304f54d85a24', '1435d1c5-0c22-459a-bd1a-ce593fba6c05', TO_DATE('2020-11-11', 'YYYY-MM-DD'), 'Kick your Habit', 'Drug and alcohol rehab.', 'contact@hometrust.com');
+
+insert into dynamic_framework_contract_sub_contractor(dynamic_framework_contract_id, subcontractor_provider_id)
+values('1d7f8fcc-aa12-4705-a6a5-0d40467e03e9', 'HOME_TRUST'),
+      ('1d7f8fcc-aa12-4705-a6a5-0d40467e03e9', 'HELPING_HANDS'),
+      ('1435d1c5-0c22-459a-bd1a-ce593fba6c05', 'HARMONY_LIVING');
--- src/main/resources/db/migration/V1_1__create_interventions_initial_tables.sql
+++ src/main/resources/db/migration/V1_1__create_interventions_initial_tables.sql
@@ -1,42 +1,69 @@
-CREATE TYPE approval_state AS ENUM ('APPROVED','ACCREDITED','UNSUCESSFUL','NOT_APPROVED');
 CREATE TYPE intervention_type AS ENUM ('SI','ACP','CRS');
-CREATE TYPE situation_type AS ENUM ('COMMUNITY','REMAND','PRE_RELEASE','CUSTODY','PSS');
-CREATE TYPE gender AS ENUM ('M','F','T');
-create table intervention (
+CREATE TYPE setting_type AS ENUM ('COMMUNITY','CUSTODY');
+CREATE TYPE rosh_levels AS ENUM ('LOW','MEDIUM','HIGH','VERY_HIGH');
+CREATE TYPE complexities AS ENUM ('LOW','MEDIUM','HIGH');
+CREATE TABLE auth_user (
+	id text NOT NULL,
+	auth_source text NOT NULL,
+	user_name text NOT NULL DEFAULT 'unknown'::text,
+	deleted bool NULL,
+	CONSTRAINT auth_user_pkey PRIMARY KEY (id)
+);
+create table nps_region (
+              id bpchar(1) not null,
+              name text,
+              primary key (id)
+);
+create table pcc_region (
+              id text not null,
+              name text,
+              nps_region bpchar(1) not null,
+              primary key (id),
+              constraint fk__prr__nps_region foreign key (nps_region) references nps_region
+);
+CREATE TABLE contract_type (
+	id uuid NOT NULL,
+	"name" text NOT NULL,
+	code varchar(10) NOT NULL,
+	CONSTRAINT contract_type_pkey PRIMARY KEY (id)
+);
+CREATE TABLE service_category (
+	id uuid NOT NULL,
+	created timestamptz NULL,
+	"name" text NOT NULL,
+	CONSTRAINT service_category_pkey PRIMARY KEY (id)
+);
+create table intervention_catalogue (
     id uuid not null,
     name text not null,
     int_type intervention_type not null,
     short_decription text not null,
     long_decription text not null,
     topic text not null,
-    state approval_state not null,
-    literacy_level text,
-    learning_disability_catered_for boolean not null,
-    equivalent_non_ldc_programme text,
+    session_detail text not null,
     commencement_date date not null,
     termination_date date not null,
-    primary key (id)
+    created_date date not null,
+    created_by text not null,
+    last_modified_date date,
+    last_modified_by text,
+    primary key (id),
+    constraint fk__ic__auth_user_id foreign key (created_by) references auth_user,
+    constraint fk__ic__auth_user_id_mod foreign key (last_modified_by) references auth_user
 );
 create table delivery_method (
     id uuid not null,
     intervention_id uuid not null,
     delivery_method_description text,
     primary key (id),
-    constraint fk__dm__intervention_id foreign key (intervention_id) references intervention
+    constraint fk__dm__intervention_id foreign key (intervention_id) references intervention_catalogue
 );
-create table delivery_method_situation (
+create table delivery_method_setting (
     id uuid not null,
     delivery_method_id uuid not null,
-    situation situation_type not null,
+    setting setting_type not null,
     primary key (id),
     constraint fk__dms__delivery_method_id foreign key (delivery_method_id) references delivery_method
-);
-create table session (
-    id uuid not null,
-    delivery_method_id uuid not null,
-    session_detail text not null,
-    primary key (id),
-    constraint fk__session__delivery_method_id foreign key (delivery_method_id) references delivery_method
 );
 create table criminogenic_need_ref (
     id uuid not null,
@@ -45,71 +72,46 @@ create table criminogenic_need_ref (
 );
 create table criminogenic_need (
     id uuid not null,
-    need_ref uuid not null,
+    need_id uuid not null,
     intervention_id uuid not null,
     primary key (id),
-    constraint fk__cn__criminogenic_need_ref foreign key (need_ref) references criminogenic_need_ref,
-    constraint fk__cn__intervention_id foreign key (intervention_id) references intervention
+    constraint fk__cn__criminogenic_need_ref foreign key (need_id) references criminogenic_need_ref,
+    constraint fk__cn__intervention_id foreign key (intervention_id) references intervention_catalogue
 );
-insert into criminogenic_need_ref (id, name) values
-('ba962a0a-d760-48a4-bdcb-d74c445edd31','Accommodation'),
-('f52f2bef-7806-447d-a0dd-869e463a0423','Employment and Education'),
-('8d586911-dd57-4de3-b43e-8bfe77a06215','Finance and Debt'),
-('1cd19745-152d-4fe4-a4d2-6fcd8537f52c','Drug Use'),
-('025215d3-bf02-433f-b8bc-90ba2e391e3a','Alcohol Use'),
-('edd87755-2b0e-4f07-838e-4f9828e570d3','Health and Wellbeing'),
-('e8442b7f-720a-43de-8273-7d1f83f96cd1','Personal Relationships and Community'),
-('b1086ca9-b51d-440f-88a5-b597852df483','Thinking, Behaviours and Attitudes'),
-('6a967d65-4576-4293-979f-889b48ca2cba','Lifestyle and Associates'),
-('71471a03-f821-4b5d-a055-e4c7c6cf70d6','Relatives and Family');
+create table special_educational_need (
+    id uuid not null,
+    intervention_id uuid not null,
+    literacy_level_guide text,
+    learning_disability_catered_for boolean not null default false,
+    equivalent_non_ldc_programme_guide text,
+    primary key (id),
+    constraint fk__sen__intervention_id foreign key (intervention_id) references intervention_catalogue
+);
 create table possible_outcome (
     id uuid not null,
     intervention_id uuid,
     outcome text not null,
     primary key (id),
-    constraint fk__po__intervention_id foreign key (intervention_id) references intervention
+    constraint fk__po__intervention_id foreign key (intervention_id) references intervention_catalogue
 );
 create table personal_eligibility (
     id uuid not null,
     intervention_id uuid,
-    min_age integer,
-    max_age integer,
+    min_age integer default 18,
+    max_age integer default 120,
+    males boolean not null,
+    females boolean not null,
     primary key (id),
-    constraint fk__po__intervention_id foreign key (intervention_id) references intervention
+    constraint fk__pe__intervention_id foreign key (intervention_id) references intervention_catalogue
 );
-create table eligibile_gender (
-    id uuid not null,
-    gender gender,
-    personal_eligibility_id uuid,
-    primary key (id),
-    constraint fk__eg__personal_eligibility_id foreign key (personal_eligibility_id) references personal_eligibility
-);
+create unique index unq__fk__pe__intervention_id
+          on personal_eligibility (intervention_id);
 create table ethnicity_ref (
     id uuid not null,
     broad_group text not null,
     name text not null,
     primary key (id)
 );
-insert into ethnicity_ref (id, broad_group, name) values
-('52e445f5-76ef-40a3-a7c1-6e8767d1658a','Asian or Asian British','Indian'),
-('2593c35b-2dd8-4aab-aa94-055c8a793ee7','Asian or Asian British','Pakistani'),
-('97b3f4e4-dc43-474c-9801-65fb737ef656','Asian or Asian British','Bangladeshi'),
-('9901c7a5-2b34-48cb-ae56-495bb0f1c4cc','Asian or Asian British','Chinese'),
-('6f207517-cce7-4f45-94a2-2a03571fa575','Asian or Asian British','Any other Asian background'),
-('b7a945c5-bd3e-4604-86e2-9acc0188acac','Black, Black British, Caribbean or African','African'),
-('90a4f5cd-49bd-4097-8607-faa379003334','Black, Black British, Caribbean or African','Caribbean'),
-('04a902c8-7c92-4c04-9c10-bbfaff998f0d','Black, Black British, Caribbean or African','Any other Black, Black British, or Caribbean background'),
-('ef27cf80-9511-4960-9669-9670171d3350','Mixed or multiple ethnic groups','White and Black Caribbean'),
-('332ee85a-7982-4f16-a08e-97f0703c6eb1','Mixed or multiple ethnic groups','White and Black African'),
-('56890bc7-0423-42e9-b71e-13fd03dc7e7b','Mixed or multiple ethnic groups','White and Asian'),
-('9470ac74-fc29-412c-8085-a4527810c328','Mixed or multiple ethnic groups','Any other Mixed or multiple ethnic background'),
-('f5feeeb6-7485-4823-ac68-96b3db2a245b','White','English, Welsh, Scottish, Northern Irish or British'),
-('c293013a-9547-43f1-93f1-e563fe2eae2b','White','Irish'),
-('6a0eb91f-ae2b-4d26-8295-e4bcc5333fb5','White','Gypsy or Irish Traveller'),
-('b3d5f511-08a7-4b31-989b-3ec8aaf1c918','White','Roma'),
-('31e0f2c2-a5fa-4f34-9a16-61b619027519','White','Any other White background'),
-('8f298dc9-c9d3-4777-b606-c6af4b0a38c3','Other ethnic group','Arab'),
-('714d36d3-e76e-4a8e-b510-fe9bc5d28e1a','Other ethnic group','Any other ethnic group');
 create table eligibile_ethnicity (
     id uuid not null,
     ethnicity_id uuid,
@@ -123,39 +125,151 @@ create table offence_type_ref (
     name text not null,
     primary key (id)
 );
-insert into offence_type_ref (id, name) values
-('1f8a7fde-b81b-490a-a9bb-fe056d5b9294','Acquisitive offence'),
-('ec6720a8-fa08-488b-82be-2496581a8491','All offences'),
-('b0517921-0d72-4f10-bea3-be10d6036e4f','Driving offence'),
-('c19708f5-85ac-43f6-bd46-fbac6f9dae2b','Extremism offence'),
-('d5cba2c9-cfa7-4d4f-be4b-896f9063cf50','Gang or Organised Crime-Related offence'),
-('18630563-e5d3-4917-a189-3accfd7e3511','General Violence (GV)'),
-('1585e943-d552-4e2c-bd5d-08229646f99c','Intimate Partner Violence (IPV)'),
-('cdca173d-8189-4c3d-9618-4b40bda56274','Sexual offence'),
-('e62edfb1-e533-480b-84c1-587711f0c259','Substance-Related offence');
 create table eligibile_offence (
     id uuid not null,
     offence_type_id uuid,
-    personal_eligibility_id uuid,
+    intervention_id uuid,
     victim_type text,
     motivation text,
     primary key (id),
     constraint fk__eo__offence_type_id foreign key (offence_type_id) references offence_type_ref,
-    constraint fk__eo__personal_eligibility_id foreign key (personal_eligibility_id) references personal_eligibility
+    constraint fk__eo__intervention_id foreign key (intervention_id) references intervention_catalogue
 );
 create table enabling_intervention (
     id uuid not null,
     intervention_id uuid,
     enabling_intervention_detail text,
     primary key (id),
-    constraint fk__ei__intervention_id foreign key (intervention_id) references intervention
+    constraint fk__ei__intervention_id foreign key (intervention_id) references intervention_catalogue
 );
 create table delivery_location (
     id uuid not null,
     intervention_id uuid,
     provider_name text not null,
     contact text not null,
-    pdu_estabishments text not null,
+    pdu_estabishments text not null, -- need R&M PDU
     primary key (id),
-    constraint fk__dl__intervention_id foreign key (intervention_id) references intervention
+    constraint fk__dl__intervention_id foreign key (intervention_id) references intervention_catalogue
+);
+create table risk_consideration (
+    id uuid not null,
+    intervention_id uuid not null,
+    cn_score_guide text,
+    extremism_risk_guide text,
+    sara_partner_score_guide text,
+    sara_other_score_guide text,
+    osp_score_guide text,
+    osp_dc_icc_combination_guide text,
+    ogrs_score_guide text,
+    ovp_guide text,
+    ogp_guide text,
+    pna_guide text,
+    rosh_level rosh_levels,
+    rsr_guide text,
+    primary key (id),
+    constraint fk__rc__intervention_id foreign key (intervention_id) references intervention_catalogue
+);
+create unique index unq__fk__rc__intervention_id
+          on risk_consideration (intervention_id);
+create table exclusion (
+    id uuid not null,
+    intervention_id uuid not null,
+    min_remaining_sentence_duration_guide text,
+    remaining_license_community_order_guide text,
+    alcohol_drug_problem_guide text,
+    mental_health_problem_guide text,
+    other_preferred_method_guide text,
+    same_type_rule_guide text,
+    schedule_frequency_guide text,
+    primary key (id),
+    constraint fk__ex__intervention_id foreign key (intervention_id) references intervention_catalogue
+);
+create unique index unq__fk__ex__intervention_id
+          on exclusion (intervention_id);
+create table excluded_offence (
+              id uuid not null,
+              offence_type_id uuid,
+              intervention_id uuid,
+              victim_type text,
+              motivation text,
+              primary key (id),
+              constraint fk__exo__offence_type_id foreign key (offence_type_id) references offence_type_ref,
+              constraint fk__exo__intervention_id foreign key (intervention_id) references intervention_catalogue
+);
+CREATE TABLE complexity_level (
+	id uuid NOT NULL,
+	title text NOT NULL,
+	description text NOT NULL,
+	service_category_id uuid NOT NULL,
+	complexity "complexities" NULL,
+	CONSTRAINT complexity_level_pkey PRIMARY KEY (id),
+	CONSTRAINT fk_service_category_id FOREIGN KEY (service_category_id) REFERENCES service_category(id)
+);
+CREATE TABLE desired_outcome (
+	id uuid NOT NULL,
+	description text NOT NULL,
+	service_category_id uuid NOT NULL,
+	CONSTRAINT desired_outcome_pkey PRIMARY KEY (id),
+	CONSTRAINT fk__desired_outcome__service_category FOREIGN KEY (service_category_id) REFERENCES service_category(id)
+);
+CREATE TABLE service_provider (
+	id varchar(30) NOT NULL,
+	name text NOT NULL,
+	CONSTRAINT service_provider_pkey PRIMARY KEY (id)
+);
+CREATE TABLE contract_type_service_category (
+	contract_type_id uuid NOT NULL,
+	service_category_id uuid NOT NULL,
+	CONSTRAINT pk_contract_type_service_category PRIMARY KEY (contract_type_id, service_category_id),
+	CONSTRAINT fk_contract_type FOREIGN KEY (contract_type_id) REFERENCES contract_type(id),
+	CONSTRAINT fk_service_category_id FOREIGN KEY (service_category_id) REFERENCES service_category(id)
+);
+CREATE TABLE dynamic_framework_contract (
+	id uuid NOT NULL,
+	prime_provider_id varchar(30) NOT NULL,
+	start_date date NOT NULL,
+	end_date date NOT NULL,
+	nps_region_id bpchar(1) NULL,
+	pcc_region_id text NULL,
+	allows_female bool NOT NULL DEFAULT true,
+	allows_male bool NOT NULL DEFAULT true,
+	minimum_age int4 NOT NULL DEFAULT 18,
+	maximum_age int4 NULL,
+	contract_reference varchar(30) NOT NULL,
+	contract_type_id uuid NOT NULL,
+	referral_start_date date NOT NULL DEFAULT '2021-01-01'::date,
+	referral_end_at timestamptz NULL,
+	CONSTRAINT dynamic_framework_contract_contract_reference_key UNIQUE (contract_reference),
+	CONSTRAINT dynamic_framework_contract_pkey PRIMARY KEY (id),
+	CONSTRAINT fk__contract_type__contract_type_id FOREIGN KEY (contract_type_id) REFERENCES contract_type(id),
+	CONSTRAINT fk__dynamic_framework_contract__nps_region_id FOREIGN KEY (nps_region_id) REFERENCES nps_region(id),
+	CONSTRAINT fk__dynamic_framework_contract__pcc_region_id FOREIGN KEY (pcc_region_id) REFERENCES pcc_region(id),
+	CONSTRAINT fk__dynamic_framework_contract__service_provider_id FOREIGN KEY (prime_provider_id) REFERENCES service_provider(id)
+);
+CREATE INDEX idx_prime_provider_id ON dynamic_framework_contract USING btree (prime_provider_id);
+CREATE TABLE dynamic_framework_contract_sub_contractor (
+	dynamic_framework_contract_id uuid NOT NULL,
+	subcontractor_provider_id varchar(30) NOT NULL,
+	CONSTRAINT pk_subcontractor PRIMARY KEY (subcontractor_provider_id, dynamic_framework_contract_id),
+	CONSTRAINT fk_contract_id FOREIGN KEY (dynamic_framework_contract_id) REFERENCES dynamic_framework_contract(id),
+	CONSTRAINT fk_subcontractor_id FOREIGN KEY (subcontractor_provider_id) REFERENCES service_provider(id)
+);
+CREATE INDEX idx_sub_contractor_prime_provider_id ON dynamic_framework_contract_sub_contractor USING btree (subcontractor_provider_id);
+CREATE TABLE intervention (
+	id uuid NOT NULL,
+	dynamic_framework_contract_id uuid NOT NULL,
+	created_at timestamptz NOT NULL,
+	title text NOT NULL,
+	description text NOT NULL,
+	incoming_referral_distribution_email text NOT NULL DEFAULT '__no_data__'::text,
+	CONSTRAINT intervention_pkey PRIMARY KEY (id),
+	CONSTRAINT fk__intervention__dynamic_framework_contract_id FOREIGN KEY (dynamic_framework_contract_id) REFERENCES public.dynamic_framework_contract(id)
+);
+CREATE UNIQUE INDEX idx_intervention_dynamic_framework_contract_id ON intervention USING btree (dynamic_framework_contract_id);
+CREATE TABLE intervention_catalogue_map (
+	intervention_catalogue_id uuid NOT NULL,
+	intervention_id uuid NOT NULL,
+	CONSTRAINT intervention_catalogue_map_pkey PRIMARY KEY (intervention_catalogue_id, intervention_id),
+	CONSTRAINT fk__icm__intervention_catalogue_id FOREIGN KEY (intervention_catalogue_id) REFERENCES intervention_catalogue(id),
+	CONSTRAINT fk__icm__intervention_id FOREIGN KEY (intervention_id) REFERENCES intervention(id)
 );
