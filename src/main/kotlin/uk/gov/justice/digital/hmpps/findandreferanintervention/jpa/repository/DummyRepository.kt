212948fd2a74118b90563c481754e96e0d1ff880
Andrew Hodgson <andrew.hodgson@digital.justice.gov.uk>
2024-12-18 10:55:39 +0000

    Remove dummy mapping causing h2 db error and point to secondary local findandrefer database in integration tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/jpa/repository/DummyRepository.kt
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.digital.hmpps.findandreferanintervention.jpa.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import uk.gov.justice.digital.hmpps.findandreferanintervention.jpa.entity.Dummy
-
-interface DummyRepository : JpaRepository<Dummy, Int> {
-  fun findByDummyId(dummyId: Int): Dummy?
-}
