52390b9477c1db6f6154b7300560b3e07c8dab5c
Andrew Hodgson <andrew.hodgson@digital.justice.gov.uk>
2024-12-20 12:00:31 +0000

    Add build step for build of data dictionary [schemaspy]

---
+++ script/build-data-dictionary.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -e
+basedir=$(dirname "$0")
+jardir="$basedir/libs"
+schemadir="$basedir/schema"
+
+mkdir -p "$jardir" "$schemadir"
+
+wget https://github.com/schemaspy/schemaspy/releases/download/v6.1.0/schemaspy-6.1.0.jar -O "$jardir/schemaspy.jar"
+wget https://jdbc.postgresql.org/download/postgresql-42.2.19.jar -O "$jardir/postgresql.jar"
+
+java -jar "$jardir/schemaspy.jar" -t pgsql -dp "$jardir/postgresql.jar" \
+  -db "${POSTGRES_DB:-findandrefer}" -host "localhost" -port "5433" -u "postgres" -p "password" \
+  -nopages \
+  -o "$schemadir"
+
+docker build --pull --file="$basedir/Dockerfile.data-dictionary" --tag=data-dictionary "$basedir"
