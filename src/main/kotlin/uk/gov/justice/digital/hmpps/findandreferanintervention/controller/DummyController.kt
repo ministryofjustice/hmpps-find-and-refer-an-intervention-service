d09a9f2a003cb8847d74a5423423f25bfb3230f2
niaevans9 <nia.evans@digital.justice.gov.uk>
2025-01-17 10:16:45 +0000

    Integrate app insights (#40)
    * Integrate app insights
    
    * Linting
    
    * Linting
    
    * Add role name to dev config

--- src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/controller/DummyController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/controller/DummyController.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.findandreferanintervention.controller
 
+import com.microsoft.applicationinsights.TelemetryClient
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,9 +13,22 @@ import uk.gov.justice.digital.hmpps.findandreferanintervention.jpa.repository.Du
 @PreAuthorize("hasRole('ROLE_FIND_AND_REFER_AN_INTERVENTION_API__FAR_UI__WR')")
 class DummyController(
   private val dummyRepository: DummyRepository,
+  private val telemetryClient: TelemetryClient,
 ) {
   @GetMapping("/dummy/{id}", produces = [MediaType.APPLICATION_JSON_VALUE])
   fun getDummy(@PathVariable id: Int): Dummy? {
+    logToAppInsights(id)
     return dummyRepository.findByDummyId(id)
+  }
+
+  fun logToAppInsights(id: Int) {
+    telemetryClient.trackEvent(
+      "LoggingToAppInsights",
+      mapOf(
+        "userMessage" to "User has hit the dummy endpoint",
+        "dummyId" to id.toString(),
+      ),
+      null,
+    )
   }
 }
