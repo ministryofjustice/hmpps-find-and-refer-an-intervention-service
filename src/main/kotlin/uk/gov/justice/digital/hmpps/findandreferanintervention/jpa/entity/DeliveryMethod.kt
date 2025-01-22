523614e958daaefcd5daef50effd866216e4acb3
Jack Dempster <jack.dempster@justice.gov.uk>
2025-01-22 16:35:04 +0000

    FRI-112: Fix jpa entity relationships. Implement DTO layer for catalogue. Expose interventions summary endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/jpa/entity/DeliveryMethod.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/jpa/entity/DeliveryMethod.kt
@@ -2,13 +2,13 @@ package uk.gov.justice.digital.hmpps.findandreferanintervention.jpa.entity
 
 import jakarta.annotation.Nullable
 import jakarta.persistence.Column
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToMany
+import jakarta.validation.constraints.NotNull
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
-import jakarta.validation.constraints.NotNull
 import java.util.UUID
 
 @Entity
