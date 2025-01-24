aaf58fcc0d36b7e2cdd6b0a289ad1045815017c6
Jack Dempster <jack.dempster@justice.gov.uk>
2025-01-24 14:01:48 +0000

    FRI-110: Add repository tests for finding paged response of interventions

--- src/test/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/integration/jpa/directory/InterventionCatalogueRepositoryTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/integration/jpa/directory/InterventionCatalogueRepositoryTest.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.findandreferanintervention.integration.jpa.directory
 
+import au.com.dius.pact.core.support.hasProperty
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,10 +18,53 @@ class InterventionCatalogueRepositoryTest @Autowired constructor(
 ) {
 
   @Test
-  fun `whenFindAll exists return list of interventions`() {
+  fun `whenFindAll with pageRequest and there are interventions return a page of interventions`() {
     val pageRequest = PageRequest.of(0, 10)
     val interventions = interventionCatalogueRepository.findAll(pageRequest)
 
+    assertThat(interventions.totalElements).isGreaterThan(0)
+    assertThat(
+      interventions.content.all {
+        it.hasProperty("name")
+        it.hasProperty("shortDescription")
+        it.hasProperty("intType")
+        it.hasProperty("criminogenicNeeds")
+        it.hasProperty("deliveryLocations")
+        it.hasProperty("deliveryMethods")
+        it.hasProperty("eligibleOffences")
+        it.hasProperty("enablingInterventions")
+        it.hasProperty("excludedOffences")
+        it.hasProperty("exclusion")
+        it.hasProperty("personalEligibility")
+        it.hasProperty("possibleOutcomes")
+        it.hasProperty("riskConsideration")
+        it.hasProperty("specialEducationalNeeds")
+      },
+    )
+  }
+
+  @Test
+  fun `whenFindAll and there are interventions return a list of interventions`() {
+    val interventions = interventionCatalogueRepository.findAll()
+
     assertThat(interventions.size).isGreaterThan(0)
+    assertThat(
+      interventions.all {
+        it.hasProperty("name")
+        it.hasProperty("shortDescription")
+        it.hasProperty("intType")
+        it.hasProperty("criminogenicNeeds")
+        it.hasProperty("deliveryLocations")
+        it.hasProperty("deliveryMethods")
+        it.hasProperty("eligibleOffences")
+        it.hasProperty("enablingInterventions")
+        it.hasProperty("excludedOffences")
+        it.hasProperty("exclusion")
+        it.hasProperty("personalEligibility")
+        it.hasProperty("possibleOutcomes")
+        it.hasProperty("riskConsideration")
+        it.hasProperty("specialEducationalNeeds")
+      },
+    )
   }
 }
