726031a6cdd7e6224d6c0d2358bbf83a42f102f6
Davansah <david.ansah@digital.justice.gov.uk>
2025-01-27 14:53:44 +0000

    FRI-122/sample_interventions_data: Added comments

--- src/main/resources/db/migration/V1_4__amend_interventions_catalogue_table.sql
+++ src/main/resources/db/migration/V1_4__amend_interventions_catalogue_table.sql
@@ -1,10 +1,12 @@
 alter table intervention_catalogue rename column short_decription to short_description
 alter table intervention_catalogue rename column long_decription to long_description
 
+-- removing constraints as data not mandatory for all intervention types
 alter table intervention_catalogue alter column long_description text null;
 alter table intervention_catalogue alter column topic text null;
 alter table intervention_catalogue alter column session_detail text null;
 alter table intervention_catalogue alter column commencement_date date null;
 alter table intervention_catalogue alter column termination_date date null;
 
+-- adding column in this table until final decision is made on where this data is displayed
 alter table intervention_catalogue add column reasons_for_referral text;
