22b570a867d5c7472dec526ea7850ff53e19bc85
Davansah <david.ansah@digital.justice.gov.uk>
2025-01-27 15:04:13 +0000

    FRI-122/sample_interventions_data: Fixed sql statement issue highlighted by PR validator

--- src/main/resources/db/migration/V1_4__amend_interventions_catalogue_table.sql
+++ src/main/resources/db/migration/V1_4__amend_interventions_catalogue_table.sql
@@ -1,5 +1,5 @@
-alter table intervention_catalogue rename column short_decription to short_description
-alter table intervention_catalogue rename column long_decription to long_description
+alter table intervention_catalogue rename column short_decription to short_description;
+alter table intervention_catalogue rename column long_decription to long_description;
 
 -- removing constraints as data not mandatory for all intervention types
 alter table intervention_catalogue alter column long_description text null;
@@ -9,4 +9,4 @@ alter table intervention_catalogue alter column commencement_date date null;
 alter table intervention_catalogue alter column termination_date date null;
 
 -- adding column in this table until final decision is made on where this data is displayed
-alter table intervention_catalogue add column reasons_for_referral text;
+alter table intervention_catalogue add column reasons_for_referral text null;
