0aa4b8d66d2946708f1fbbe2f0855572d790f25e
Jack Dempster <5928690+Jack-Dempster@users.noreply.github.com>
2025-02-03 15:54:57 +0000

    Fri 112/create interventions catalogue endpoint (#49)
    * FRI-110: Add JPA entities for tables required for interventions summary
    
    * FRI-110: Update intervention catalogue entity to include new fields
    
    * Fri 95/add preprod build (#42)
    
    * Add preprod build step
    
    * remove approval step and sequence after dev deploy
    
    * Remove ref to template project
    
    * Add helm deploy details and app insights for preprod env.
    
    * FRI-111: Create DTOs for interventions summary
    
    * FRI-111: Update intervention catalogue dto to include reasonsForReferral field
    
    * FRI-110: Add JPA entities for tables required for interventions summary
    
    * FRI-110: remove explicitly defined uri for postgres locally
    
    * FRI-112: Fix jpa entity relationships. Implement DTO layer for catalogue. Expose interventions summary endpoint
    
    * FRI-112: Add interventions summary controller tests
    
    * FRI-112: Fix delivery_method_settings relationship + insert default ages for personal_eligibility rather than null
    
    * FRI-112: Change local data insertions from blank string -> null
    
    ---------
    
    Co-authored-by: Andrew Hodgson <115979893+awhodgson256@users.noreply.github.com>

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/service/InterventionCatalogueService.kt
@@ -0,0 +1,18 @@
+package uk.gov.justice.digital.hmpps.findandreferanintervention.service
+
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.findandreferanintervention.dto.InterventionCatalogueDto
+import uk.gov.justice.digital.hmpps.findandreferanintervention.jpa.repository.InterventionCatalogueRepository
+
+@Service
+class InterventionCatalogueService(
+  val interventionCatalogueRepository: InterventionCatalogueRepository,
+) {
+
+  fun getInterventionsCatalogue(pageable: Pageable): Page<InterventionCatalogueDto> {
+    return interventionCatalogueRepository.findAll(pageable)
+      .map { intervention -> InterventionCatalogueDto.fromEntity(intervention) }
+  }
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/integration/controller/InterventionCatalogueControllerTest.kt
@@ -0,0 +1,87 @@
+package uk.gov.justice.digital.hmpps.findandreferanintervention.integration.controller
+
+import au.com.dius.pact.core.support.hasProperty
+import com.microsoft.applicationinsights.TelemetryClient
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.PageRequest
+import uk.gov.justice.digital.hmpps.findandreferanintervention.controller.InterventionCatalogueController
+import uk.gov.justice.digital.hmpps.findandreferanintervention.dto.InterventionCatalogueDto
+import uk.gov.justice.digital.hmpps.findandreferanintervention.jpa.entity.InterventionType
+import uk.gov.justice.digital.hmpps.findandreferanintervention.jpa.entity.SettingType
+import uk.gov.justice.digital.hmpps.findandreferanintervention.service.InterventionCatalogueService
+import java.util.UUID
+
+internal class InterventionCatalogueControllerTest {
+  private val telemetryClient = mock<TelemetryClient>()
+  private val interventionCatalogueService = mock<InterventionCatalogueService>()
+  private val interventionCatalogueController =
+    InterventionCatalogueController(interventionCatalogueService, telemetryClient)
+
+  @Test
+  fun `getInterventionsCatalogue when present return a paged result of interventions`() {
+    val pageable = PageRequest.of(0, 10)
+    val catalogue = InterventionCatalogueDto(
+      id = UUID.randomUUID(),
+      title = "Test Title",
+      description = "Test Description",
+      deliveryFormat = listOf("In Person"),
+      interventionType = InterventionType.ACP,
+      setting = listOf(SettingType.COMMUNITY),
+      allowsMales = true,
+      allowsFemales = true,
+      minAge = 18,
+      maxAge = 30,
+      riskCriteria = listOf("RISK_CRITERIA_1", "RISK_CRITERIA_2"),
+      attendanceType = listOf("One-to-one"),
+    )
+    whenever(interventionCatalogueService.getInterventionsCatalogue(pageable)).thenReturn(PageImpl(listOf(catalogue)))
+    val response = interventionCatalogueController.getInterventionsCatalogue(pageable)
+
+    verify(telemetryClient).trackEvent(
+      "InterventionsCatalogue Summary",
+      mapOf("userMessage" to "User has hit interventions catalogue summary page"),
+      null,
+    )
+
+    assertThat(response).isNotNull
+    assertThat(response.content).isNotEmpty
+    assertThat(
+      response.content.all {
+        it.hasProperty("id")
+        it.hasProperty("title")
+        it.hasProperty("description")
+        it.hasProperty("deliveryFormat")
+        it.hasProperty("interventionTyps")
+        it.hasProperty("setting")
+        it.hasProperty("allowsMales")
+        it.hasProperty("allowsFemales")
+        it.hasProperty("minAge")
+        it.hasProperty("maxAge")
+        it.hasProperty("riskCriteria")
+        it.hasProperty("attendanceType")
+      },
+    )
+    assertThat(response.content[0].title).isEqualTo("Test Title")
+  }
+
+  @Test
+  fun `getInterventionsCatalogue when empty return a empty list of interventions`() {
+    val pageable = PageRequest.of(0, 10)
+    whenever(interventionCatalogueService.getInterventionsCatalogue(pageable)).thenReturn(PageImpl(listOf()))
+    val response = interventionCatalogueController.getInterventionsCatalogue(pageable)
+
+    verify(telemetryClient).trackEvent(
+      "InterventionsCatalogue Summary",
+      mapOf("userMessage" to "User has hit interventions catalogue summary page"),
+      null,
+    )
+
+    assertThat(response).isNotNull
+    assertThat(response.content).isEmpty()
+  }
+}
