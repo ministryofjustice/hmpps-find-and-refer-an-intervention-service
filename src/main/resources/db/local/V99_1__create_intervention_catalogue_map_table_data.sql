0e29dde5d626532d4ae5e982e8eb5003d44c62a1
Davansah <david.ansah@digital.justice.gov.uk>
2025-02-13 13:59:22 +0000

    FRI-277: Added SQL Scripts to remove Catalogue data from migration and to be ran locally

--- src/main/resources/db/local/V99_1__create_intervention_catalogue_map_table_data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO intervention_catalogue_map (intervention_catalogue_id, intervention_id)
-VALUES
-    ('dd746a25-09b0-4d86-96ab-79e3539593a2','98a42c61-c30f-4beb-8062-04033c376e2d'),
-    ('7c479daa-9dd2-4307-a7e7-2e2ff4c3ced1','f803445f-326c-4ef8-aee2-f7716d417832'),
-    ('ce0bf924-d5eb-498f-9376-8a01a07510f5','3ccb511b-89b2-42f7-803b-304f54d85a24');
