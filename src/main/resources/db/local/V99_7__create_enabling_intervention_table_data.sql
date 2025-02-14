96510afbcf749939c9cafcf06eea26ae785a9668
Davansah <david.ansah@digital.justice.gov.uk>
2025-02-14 11:28:02 +0000

    FRI-277: Removed empty records, replaced empty strings as null

---
+++ src/main/resources/db/local/V99_7__create_enabling_intervention_table_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO enabling_intervention (id, intervention_id, enabling_intervention_detail)
+VALUES  ('21da1c10-16ba-478f-a143-ac42845666a4','4902a268-9907-4070-ba48-7c2870a3b77e',null),
+        ('a670ada4-b10b-4e34-834f-ac6464c55f61','d61fc961-243e-4321-bafb-5d71e1fd13a4',null),
+        ('c34a6e04-6925-4557-b03c-9fe43917c2ef','432b2353-b794-4b12-8018-6c7ede6a1ed6','Kaizen, Horizon, BNM+ or NMS'),
+        ('c7f6352e-df07-4d32-876b-13ffcf04f3fb','a6c18ff3-83bd-4ad2-bcbe-1db3fb55a341',null),
+        ('dc093732-eff6-47eb-a74f-f4d982556dca','94af0e1f-f55f-4422-afc5-536178c63b5f',null),
+        ('46e042f7-e29d-4b5c-9095-e420e4df5f1d','dd746a25-09b0-4d86-96ab-79e3539593a2',null),
+        ('ea2b38b0-6cee-4b5d-bb63-64086282fb9b','ecb90387-86f1-47d1-ae78-e987b408b0b6',null),
+        ('aa325372-dbb7-4595-85ff-d730aee055b9','7c479daa-9dd2-4307-a7e7-2e2ff4c3ced1',null),
+        ('af9d2268-4876-4983-9693-2e5839c253b4','ce0bf924-d5eb-498f-9376-8a01a07510f5',null);
