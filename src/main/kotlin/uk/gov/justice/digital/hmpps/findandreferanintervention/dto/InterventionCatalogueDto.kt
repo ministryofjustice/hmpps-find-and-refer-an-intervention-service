f6c84cc066de9fab5b2fca749adfd4a696efafac
Jack Dempster <jack.dempster@justice.gov.uk>
2025-02-14 11:04:32 +0000

    remove slashes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/dto/InterventionCatalogueDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/dto/InterventionCatalogueDto.kt
@@ -9,18 +9,18 @@ import java.util.UUID
 // Excludes any properties that have null values when creating dto.
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class InterventionCatalogueDto(
-  val id: UUID, //
-  val criminogenicNeeds: List<String>, //
-  val title: String, //
-  val description: String, //
-  val interventionType: InterventionType, //
-  val setting: List<SettingType>, //
-  val allowsMales: Boolean, //
-  val allowsFemales: Boolean, //
-  val riskCriteria: List<String>?, //
-  val attendanceType: List<String>, //
-  val deliveryFormat: List<String>, //
-  val timeToComplete: String?, //
+  val id: UUID,
+  val criminogenicNeeds: List<String>,
+  val title: String,
+  val description: String,
+  val interventionType: InterventionType,
+  val setting: List<SettingType>,
+  val allowsMales: Boolean,
+  val allowsFemales: Boolean,
+  val riskCriteria: List<String>?,
+  val attendanceType: List<String>,
+  val deliveryFormat: List<String>,
+  val timeToComplete: String?,
   val suitableForPeopleWithLearningDifficulties: Boolean?,
   val equivalentNonLdcProgramme: String?,
 ) {
