ca4c7d55bc870419bbc25c5047e3e70f736375dd
Davansah <david.ansah@digital.justice.gov.uk>
2025-02-14 11:45:29 +0000

    FRI-277: Added SQL Scripts to remove Intervention Catalogue data from DEV (#73)
    * FRI-277: Added SQL Scripts to remove Catalogue data from migration and to be ran locally

---
+++ src/main/resources/db/migration/V1_30__remove_intervention_catalogue_data.sql
@@ -0,0 +1,14 @@
+DELETE FROM special_educational_need;
+DELETE FROM risk_consideration;
+DELETE FROM possible_outcome;
+DELETE FROM personal_eligibility;
+DELETE FROM criminogenic_need;
+DELETE FROM exclusion;
+DELETE FROM enabling_intervention;
+DELETE FROM eligible_offence;
+DELETE FROM delivery_method_setting;
+DELETE FROM delivery_method;
+DELETE FROM delivery_location;
+DELETE FROM intervention_catalogue_map;
+DELETE FROM intervention_catalogue;
+DELETE FROM auth_user;
