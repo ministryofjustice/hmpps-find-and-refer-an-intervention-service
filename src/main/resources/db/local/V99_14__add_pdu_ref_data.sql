ce56bfdb36d6d33a20e06927df2cbcbb2ea29550
Jack Dempster <jack.dempster@justice.gov.uk>
2025-02-19 10:11:18 +0000

    FRI-257: Create PDU reference data table

---
+++ src/main/resources/db/local/V99_14__add_pdu_ref_data.sql
@@ -0,0 +1,9 @@
+-- sample records to insert for testing
+insert into pdu_ref(id, name, pcc_region_id)
+values ('redcar-cleveland-and-middlesbrough', 'Redcar, Cleveland and Middlesbrough', 'cleveland'),
+       ('stockton-and-hartlepool', 'Stockton and Hartlepool', 'cleveland'),
+       ('county-durham-and-darlington', 'County Durham and Darlington', 'durham'),
+       ('newcastle-upon-tyne', 'Newcastle Upon Tyne', 'northumbria'),
+       ('gateshead-and-south-tyneside', 'Gateshead and South Tyneside', 'northumbria'),
+       ('Sunderland', 'Sunderland', 'northumbria'),
+       ('north-tyneside-and-northumberland', 'North Tyneside and Northumberland', 'northumbria');
---
+++ src/main/resources/db/migration/V1_32__add_pdu_reference_data_table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE pdu_ref
+(
+    id            text NOT NULL,
+    name          text NOT NULL,
+    pcc_region_id text NOT NULL,
+    CONSTRAINT pdu_pkey PRIMARY KEY (id),
+    CONSTRAINT pdu_name_key UNIQUE (name),
+    CONSTRAINT fk__pdu__pcc_region_id FOREIGN KEY (pcc_region_id)
+        REFERENCES pcc_region (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+
+);
+
+COMMENT ON TABLE pdu_ref IS '**reference data** Probation Delivery Unit (PDU) details';
+
+COMMENT ON COLUMN pdu_ref.id IS 'PDU unique identifier';
+
+COMMENT ON COLUMN pdu_ref.name IS 'PDU name';
