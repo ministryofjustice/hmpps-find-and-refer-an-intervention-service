7466167e560b99373ae8bdd58d833cec4f160bd5
Jack Dempster <5928690+Jack-Dempster@users.noreply.github.com>
2025-02-19 11:59:00 +0000

    FRI-257: Create PDU reference data table (#77)

---
+++ src/main/resources/db/migration/V1_32__add_pdu_reference_data_table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE pdu_ref
+(
+    id            text NOT NULL,
+    name          text NOT NULL,
+    pcc_region_id text NOT NULL,
+    CONSTRAINT pdu_pkey PRIMARY KEY (id),
+    CONSTRAINT pdu_name_key UNIQUE (name),
+    CONSTRAINT fk__pdu__pcc_region_id FOREIGN KEY (pcc_region_id) REFERENCES pcc_region (id)
+
+);
+
+COMMENT ON TABLE pdu_ref IS '**reference data** Probation Delivery Unit (PDU) details';
+COMMENT ON COLUMN pdu_ref.id IS 'PDU unique identifier';
+COMMENT ON COLUMN pdu_ref.name IS 'PDU name';
+COMMENT ON COLUMN pdu_ref.pcc_region_id IS 'ID of the PCC region that the PDU is related to';
+
+INSERT INTO metadata (table_name, column_name, sensitive, domain_data)
+VALUES ('pdu_ref', 'id', 'FALSE', 'TRUE'),
+       ('pdu_ref', 'name', 'FALSE', 'TRUE'),
+       ('pdu_ref', 'pcc_region_id', 'FALSE', 'TRUE');
