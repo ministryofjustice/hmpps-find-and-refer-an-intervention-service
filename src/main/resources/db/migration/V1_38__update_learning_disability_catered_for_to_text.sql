06f334295c388114c4f158cc6e3a34fd1e376c6a
Jack Dempster <5928690+Jack-Dempster@users.noreply.github.com>
2025-03-14 10:08:10 +0000

    remove postegres.public from flyway migration causing deployment to dev to fail (#99)

--- src/main/resources/db/migration/V1_38__update_learning_disability_catered_for_to_text.sql
+++ src/main/resources/db/migration/V1_38__update_learning_disability_catered_for_to_text.sql
@@ -1,4 +1,4 @@
-ALTER TABLE postgres.public.special_educational_need
+ALTER TABLE special_educational_need
     ALTER COLUMN learning_disability_catered_for DROP NOT NULL,
     ALTER COLUMN learning_disability_catered_for TYPE TEXT USING
         CASE
