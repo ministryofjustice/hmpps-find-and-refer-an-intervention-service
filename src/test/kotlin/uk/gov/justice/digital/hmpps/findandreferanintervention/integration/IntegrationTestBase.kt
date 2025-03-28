de138b4ceeb54afaf6a0f1ebc36e5be1a8cdbfbc
Jack Dempster <jack.dempster@justice.gov.uk>
2025-03-28 16:49:23 +0000

    FRI-451: Remove local profile from intergrationTestBase class

--- src/test/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/integration/IntegrationTestBase.kt
@@ -15,7 +15,7 @@ import uk.gov.justice.hmpps.test.kotlin.auth.JwtAuthorisationHelper
 
 @ExtendWith(HmppsAuthApiExtension::class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-@ActiveProfiles("integration", "local")
+@ActiveProfiles("integration")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @EnableTransactionManagement
 abstract class IntegrationTestBase {
