1133f2642d782e34db5f2e25dc664ed806b3da55
Davansah <david.ansah@digital.justice.gov.uk>
2025-04-23 12:56:40 +0100

    FRI-509: Updated code and tests for upsertPossibleOutcomes functionality

--- src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/jpa/entity/PossibleOutcome.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/jpa/entity/PossibleOutcome.kt
@@ -1,8 +1,12 @@
 package uk.gov.justice.digital.hmpps.findandreferanintervention.jpa.entity
 
+import jakarta.annotation.Nullable
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
 import jakarta.validation.constraints.NotNull
 import uk.gov.justice.digital.hmpps.findandreferanintervention.dto.PossibleOutcomeDto
@@ -19,6 +23,11 @@ open class PossibleOutcome(
   @NotNull
   @Column(name = "outcome", length = Integer.MAX_VALUE)
   open var outcome: String,
+
+  @Nullable
+  @OneToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "intervention_id", referencedColumnName = "id")
+  open var intervention: InterventionCatalogue?,
 )
 
 fun PossibleOutcome.toDto(): PossibleOutcomeDto = PossibleOutcomeDto(
