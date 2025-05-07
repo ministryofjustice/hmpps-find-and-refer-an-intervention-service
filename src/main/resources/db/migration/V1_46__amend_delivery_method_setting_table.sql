7e14451d6fd387c0cd309abef65db7c742468a0d
Davansah <david.ansah@digital.justice.gov.uk>
2025-05-07 15:24:48 +0100

    FRI-526: Amended Migration to update Metadata table

--- src/main/resources/db/migration/V1_46__amend_delivery_method_setting_table.sql
+++ src/main/resources/db/migration/V1_46__amend_delivery_method_setting_table.sql
@@ -8,3 +8,8 @@ ALTER TABLE delivery_method_setting
     ADD CONSTRAINT fk__dms__intervention_id FOREIGN KEY (intervention_id) REFERENCES intervention_catalogue;
 
 COMMENT ON COLUMN delivery_method_setting.intervention_id IS 'the ID of the intervention delivery method setting belongs to';
+
+UPDATE metadata
+   SET column_name = 'intervention_id'
+WHERE table_name = 'delivery_method_setting'
+   AND column_name = 'delivery_method_id';
