610121b7f64622297748ba9a035bc87a7efe9fc8
kumar.thangavel <kumar.thangavel1@justice.gov.uk>
2025-05-22 16:53:22 +0100

    prevent the upsert intervention job to run on startup

--- src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/jobs/scheduled/loadinterventions/UpsertInterventionsJobConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/jobs/scheduled/loadinterventions/UpsertInterventionsJobConfiguration.kt
@@ -2,17 +2,12 @@ package uk.gov.justice.digital.hmpps.findandreferanintervention.jobs.scheduled.l
 
 import mu.KLogging
 import org.springframework.batch.core.Job
-import org.springframework.batch.core.JobParameters
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.job.builder.JobBuilder
-import org.springframework.batch.core.launch.JobLauncher
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.item.ItemReader
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.boot.ApplicationRunner
-import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
@@ -28,23 +23,6 @@ class UpsertInterventionsJobConfiguration(
   @Qualifier("jobRepository") private val jobRepository: JobRepository,
 ) {
   companion object : KLogging()
-
-  @Bean
-  fun upsertInterventionsJobLauncher(upsertInterventionsJob: Job): ApplicationRunner = onStartupJobLauncherFactory.makeBatchLauncher(upsertInterventionsJob)
-
-  @Bean
-  fun jobLauncherCommandlineRunner(jobLauncher: JobLauncher, upsertInterventionsJob: Job): CommandLineRunner = CommandLineRunner {
-    try {
-      jobLauncher.run(upsertInterventionsJob, JobParameters())
-    } catch (e: JobExecutionAlreadyRunningException) {
-      // TODO: Better handle this use case, by cancelling the job that's running (if it's long-lived)
-      logger.error("Job already running, skipping execution", e)
-    } catch (e: Exception) {
-      logger.error("Error running job: ${e.message}", e)
-    } finally {
-      logger.info("Job completed")
-    }
-  }
 
   @Bean
   fun upsertInterventionsJob(upsertInterventionsStep: Step, jobRepository: JobRepository): Job {
