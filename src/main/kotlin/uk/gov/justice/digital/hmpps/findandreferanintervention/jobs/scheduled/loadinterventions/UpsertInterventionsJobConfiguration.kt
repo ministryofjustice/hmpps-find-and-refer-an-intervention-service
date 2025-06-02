5131cc0ab15ba604ce84e27e89b25cd5625ffd6a
Kumar Thangavel <kumar.thangavel1@justice.gov.uk>
2025-06-02 13:29:02 +0100

    integrating limited access offender check (#166)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/jobs/scheduled/loadinterventions/UpsertInterventionsJobConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/jobs/scheduled/loadinterventions/UpsertInterventionsJobConfiguration.kt
@@ -2,15 +2,21 @@ package uk.gov.justice.digital.hmpps.findandreferanintervention.jobs.scheduled.l
 
 import mu.KLogging
 import org.springframework.batch.core.Job
+import org.springframework.batch.core.JobParametersBuilder
 import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.JobRegistry
 import org.springframework.batch.core.job.builder.JobBuilder
+import org.springframework.batch.core.launch.JobLauncher
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.item.ItemReader
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import uk.gov.justice.digital.hmpps.findandreferanintervention.jobs.scheduled.OnStartupJobLauncherFactory
@@ -27,6 +33,25 @@ class UpsertInterventionsJobConfiguration(
 
   @Bean
   fun upsertInterventionsJobLauncher(upsertInterventionsJob: Job): ApplicationRunner = onStartupJobLauncherFactory.makeBatchLauncher(upsertInterventionsJob)
+
+  @Bean
+  @Profile("local")
+  fun jobLauncherCommandlineRunner(jobLauncher: JobLauncher, jobRegistry: JobRegistry): CommandLineRunner = CommandLineRunner { args ->
+    try {
+      val jobNames = listOf("upsertInterventionsJob", "loadDeliveryLocationJob", "loadInterventionCatalogueMapJob", "loadInterventionCatalogueToCourseMapJob")
+      jobNames.forEach { jobName ->
+        val job = jobRegistry.getJob(jobName)
+        logger.info("Running job: $jobName")
+        jobLauncher.run(job, JobParametersBuilder().addLong("time", System.currentTimeMillis()).toJobParameters())
+      }
+    } catch (e: JobExecutionAlreadyRunningException) {
+      logger.error("Job already running, skipping execution", e)
+    } catch (e: Exception) {
+      logger.error("Error running job: ${e.message}", e)
+    } finally {
+      logger.info("Job execution completed")
+    }
+  }
 
   @Bean
   fun upsertInterventionsJob(upsertInterventionsStep: Step, jobRepository: JobRepository): Job {
