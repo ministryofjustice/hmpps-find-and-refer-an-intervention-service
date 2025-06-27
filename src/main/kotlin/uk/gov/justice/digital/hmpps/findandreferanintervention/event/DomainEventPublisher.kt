aefd6507230f700f83aa25333d7e286a57684fe0
Anand Elangovan <anand.elangovan@digital.justice.gov.uk>
2025-06-27 14:53:46 +0100

    [APG-1093] Groundword to publish message to hmpps domain events queue

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/findandreferanintervention/event/DomainEventPublisher.kt
@@ -0,0 +1,20 @@
+package uk.gov.justice.digital.hmpps.findandreferanintervention.event
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.stereotype.Service
+import uk.gov.justice.hmpps.sqs.HmppsQueueService
+import uk.gov.justice.hmpps.sqs.publish
+
+@Service
+class DomainEventPublisher(
+    private val hmppsQueueService: HmppsQueueService,
+    private val objectMapper: ObjectMapper,
+) {
+    private val domainEventsTopic by lazy {
+        hmppsQueueService.findByTopicId("hmppseventtopic") ?: throw IllegalStateException("hmppseventtopic not found")
+    }
+
+    fun publishSingle(domainEvent: DomainEvent) {
+        domainEventsTopic.publish(domainEvent.eventType, objectMapper.writeValueAsString(domainEvent))
+    }
+}
